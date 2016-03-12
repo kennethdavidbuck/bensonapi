d443ca3d89ccd243f0ea511cb0044769e35c4654
Kenneth Buck <kenneth@nestedsphere.com>
2016-03-12 18:23:44 -0500

    add basic verbs and method handlers to PostController

--- src/main/java/com/kennethbuck/bensonapi/web/PostController.java
+++ src/main/java/com/kennethbuck/bensonapi/web/PostController.java
@@ -2,13 +2,35 @@ package com.kennethbuck.bensonapi.web;
 
 import com.kennethbuck.bensonapi.domain.Post;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/posts")
 public class PostController {
 
-    @RequestMapping("/api/posts")
+    @RequestMapping(method = RequestMethod.GET)
     public Post[] index() {
         return new Post[]{new Post(1, "Teaser", "Body", "Title")};
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Post show() {
+        return new Post(1, "Teaser", "Body", "Title");
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void create() {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH)
+    public void update() {
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void delete() {
+
     }
 }
