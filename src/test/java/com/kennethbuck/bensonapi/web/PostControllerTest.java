d88768918a618978aa07c186aa40d446512151a6
Kenneth Buck <kenneth@nestedsphere.com>
2016-03-12 16:27:55 -0500

    create PostController and map to api/posts endpoint. write initial test to demonstrate endpoint can be accessed

---
+++ src/test/java/com/kennethbuck/bensonapi/web/PostControllerTest.java
@@ -0,0 +1,34 @@
+package com.kennethbuck.bensonapi.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class PostControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new PostController()).build();
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/posts").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
