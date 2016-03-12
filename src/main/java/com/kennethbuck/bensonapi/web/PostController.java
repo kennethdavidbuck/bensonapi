aa99acc4e21c7e540fee905c39dcb37a3e9b0c08
Kenneth Buck <kenneth@nestedsphere.com>
2016-03-12 18:03:59 -0500

    changed posts endpoint to return example array with single post

--- src/main/java/com/kennethbuck/bensonapi/web/PostController.java
+++ src/main/java/com/kennethbuck/bensonapi/web/PostController.java
@@ -1,5 +1,6 @@
 package com.kennethbuck.bensonapi.web;
 
+import com.kennethbuck.bensonapi.domain.Post;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     @RequestMapping("/api/posts")
-    public String index() {
-        return "api/posts";
+    public Post[] index() {
+        return new Post[]{new Post(1, "Teaser", "Body", "Title")};
     }
 }
