250f64f93028d9c4aef5b6f49674c5b823e3d7cb
Kenneth Buck <kenneth@nestedsphere.com>
2016-04-30 12:16:43 -0400

    uncomment test

--- src/test/java/com/kennethbuck/bensonapi/web/HomeControllerTest.java
+++ src/test/java/com/kennethbuck/bensonapi/web/HomeControllerTest.java
@@ -4,11 +4,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
@@ -24,8 +28,7 @@ public class HomeControllerTest {
 
     @Test
     public void getHome() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(equalTo("Benson API v0.1.0")));
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
